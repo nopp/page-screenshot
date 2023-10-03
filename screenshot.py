fa08de638c5a22379c6ce808bb341886937a0999
Carlos Augusto Malucelli <camalucelli@gmail.com>
2023-10-03 10:45:18 -0300

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,3 +1,13 @@
 import imgkit
+import argparse
 
-imgkit.from_url("http://google.com","out.png")
+parser = argparse.ArgumentParser(description="""
+This script is going to create screenshot from site . 
+""")
+parser.add_argument("url", help="Url from site")
+
+args = parser.parse_args()
+
+URL = args.url
+
+imgkit.from_url(URL,"out.png")
